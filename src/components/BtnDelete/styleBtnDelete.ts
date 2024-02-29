c06f120561a6a92a0a5ba3e01dee4797245c2e8a
Pedro Negrao <macedopedro117@yahoo.com.br>
2024-02-29 00:51:11 -0300

    Exclusão de um component que não é mais necessário

--- src/components/BtnDelete/styleBtnDelete.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const ButtonDelete = styled.button`
-  color: var(--color-red);
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-`;
