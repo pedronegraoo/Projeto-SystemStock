067df386e83c6eea9db27ba84d31de3f769991bc
Pedro Negrao <macedopedro117@yahoo.com.br>
2024-03-02 17:39:14 -0300

    Adicionando o estado false dos toasts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,30 @@
 import * as S from "./styleHeader";
 import { Link } from "react-router-dom";
 import DropDownMenu from "../DropDown/DropDown";
+import useStock from "../../hooks/useStock";
+import { ProviderAllProps } from "../../context/Provider";
 // import OffCanvasMobile from "../OffCanvasMobile/OffCanvasMobile";
 
 function Header() {
+  const { setShowToastCreate, setShowToastUpdate, setShowToastRepeatedName } =
+    useStock() as ProviderAllProps;
+
   return (
     <S.HeaderStyled>
-      <Link className="nameSytem" to="/">
+      <Link className="nameSytem" to="/"  onClick={() => {
+                setShowToastCreate(false);
+                setShowToastUpdate(false);
+                setShowToastRepeatedName(false);
+              }}>
         System Stock
       </Link>
 
       <S.Nav>
-        <Link className="linkHeader" to="/">
+        <Link className="linkHeader" to="/"  onClick={() => {
+                setShowToastCreate(false);
+                setShowToastUpdate(false);
+                setShowToastRepeatedName(false);
+              }}>
           Home
         </Link>
         <Link className="linkHeader" to="/products">
