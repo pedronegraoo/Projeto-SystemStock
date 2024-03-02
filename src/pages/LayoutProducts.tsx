c74e72b85b8c8ea0fe9b3350cb4089a851b4c38e
Pedro Negrao <macedopedro117@yahoo.com.br>
2024-03-02 00:50:36 -0300

    Tipando variáveis | Criação do component input para filtrar os produtos

--- src/pages/LayoutProducts.tsx
+++ src/pages/LayoutProducts.tsx
@@ -1,7 +1,16 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
 import * as S from "../styles/LayoutProducts";
+import InputFilter from "../components/InputFilter/InputFilter";
+import useStock from "../hooks/useStock";
+
+interface StockContextData {
+  setShowToastCreate: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowToastUpdate: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 function LayoutProducts() {
+  const { setShowToastCreate, setShowToastUpdate } =
+    useStock() as StockContextData;
   const { pathname } = useLocation();
 
   return (
@@ -9,22 +18,34 @@ function LayoutProducts() {
       <S.WrapperContentLayoutProducts>
         <h1>Estoque Produtos</h1>
 
-        <S.WrapperLink>
-          <Link
-            to="/products"
-            className={`linkLayout ${pathname === "/products" ? "active" : ""}`}
-          >
-            Todos os produtos
-          </Link>
-          <Link
-            to="/products/new"
-            className={`linkLayout ${
-              pathname === "/products/new" ? "active" : ""
-            }`}
-          >
-            Novo produto
-          </Link>
-        </S.WrapperLink>
+        <S.WrapperLinkInput>
+          <S.WrapperLink>
+            <Link
+              to="/products"
+              className={`linkLayout ${
+                pathname === "/products" ? "active" : ""
+              }`}
+              onClick={() => {
+                setShowToastCreate(false);
+                setShowToastUpdate(false);
+              }}
+            >
+              Todos os produtos
+            </Link>
+            <Link
+              to="/products/new"
+              className={`linkLayout ${
+                pathname === "/products/new" ? "active" : ""
+              }`}
+            >
+              Novo produto
+            </Link>
+          </S.WrapperLink>
+
+          <S.WrapperInputFilter>
+            {pathname === "/products" ? <InputFilter /> : ""}
+          </S.WrapperInputFilter>
+        </S.WrapperLinkInput>
       </S.WrapperContentLayoutProducts>
 
       <Outlet />
